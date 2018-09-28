a8120847d29e63c242217eba0a1c7ac5f03bd7a2
Olga Sorokina <sorokinaog@icloud.com>
2018-09-28 19:41:53 -0400

    Added Username and email preserved in the signup form

--- main.py
+++ main.py
@@ -8,39 +8,40 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    encoded_error = request.args.get("error")
-    return render_template("signup_form.html", error=encoded_error)
+    error = request.args.get("error")
+    return render_template("signup_form.html", error=error) 
 
 @app.route("/signup", methods=['POST'])
 def signup():
     username = request.form['username']
     password = request.form['password']
     verify_password = request.form['verify_password']
-    email = request.form['email']   
+    email = request.form['email'] 
+    
     if username =="":
        error = "The username is required"
-       return redirect("/?error="+error)
+       return render_template("signup_form.html", error=error, username=username, email=email)
     if not valid_credentials(username):
        error = "The username is not valid"
-       return redirect("/?error="+error)
+       return render_template("signup_form.html", error=error, username="", email=email)
     if password =="":
        error = "The password is required"
-       return redirect("/?error="+error)
+       return render_template("signup_form.html", error=error, username=username, email=email)
     if not valid_credentials(password):
        error = "The password is not valid"
-       return redirect("/?error="+error)
+       return render_template("signup_form.html", error=error, username=username, email=email)
     if verify_password =="":
        error = "This fild is required"
-       return redirect("/?error="+error)
+       return render_template("signup_form.html", error=error, username=username, email=email)
     if password != verify_password:
        error = "The password does not match"
-       return redirect("/?error="+error)
+       return render_template("signup_form.html", error=error, username=username, email=email)
     if email != "" and not valid_email(email):
        error = "The email is not valid"
-       return redirect("/?error="+error) 
+       return render_template("signup_form.html", error=error, username=username, email="") 
+           
  
     return render_template("welcome.html", username=username)
 
-    
 
 app.run()
